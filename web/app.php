bd1eb8c67127741d2536f5ad2f918331f3d25139
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2018-07-24 17:12:12 +0900

    Add umask

--- web/app.php
+++ web/app.php
@@ -2,6 +2,10 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+// if you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+umask(0002);
+
 /**
  * @var Composer\Autoload\ClassLoader
  */
